bf9a72b49202e58c808225df620b5b50b35a0f72
Ajibare <ajibare.jide@gmail.com>
2025-11-11 11:29:11 +0100

    product

--- controllers/commissionController.js
+++ controllers/commissionController.js
@@ -256,3 +256,4 @@ const commissionController = {
 
 
 export default commissionController;
+
--- routes/commission.js
+++ routes/commission.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { check } from 'express-validator';
-import * as commissionController from '../controllers/commissionController.js';
+// import * as commissionController from '../controllers/commissionController.js';
+import { authenticate as verifyToken, authorize } from '../middleware/auth.js';
 import { authenticate as auth } from '../middleware/auth.js';
 import { admin } from '../middleware/auth.js';
-import { addCommission } from '../controllers/commissionController.js';
+// import { addCommission } from '../controllers/commissionController.js';
+import commissionController from '../controllers/commissionController.js';
 
 
 const router = express.Router();
@@ -57,7 +59,8 @@ router.use(verifyToken);
 router.use(authorize('admin'));
 
 // Add commission to user
-router.post('/', addCommission);
+// router.post('/', addCommission);
+router.post('/', commissionController.addCommission);
 
 
 export default router;
